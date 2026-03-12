c0cd290bec14bd56692e3d32e70760c74f8ccba5
Killaholic2502 <duyanhmai23800@gmail.com>
2026-03-12 07:48:32 +0700

    Add files via upload

---
+++ Slide1/src/Slide/Main.java
@@ -0,0 +1,11 @@
+package Slide;
+
+public class Main {
+public static void main(String[] args) {
+	Xe xe = new Xe();
+	xe.sethangXe("Toyota");
+	xe.setnamSanXuat(2025);
+	xe.xuat();
+	System.out.println("2 PS48692 Mai Duy Anh ");
+}
+}
